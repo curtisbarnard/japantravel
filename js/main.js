858d394e48ff42bbc010ee363400dfe6c81152a7
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-04-10 20:45:50 -0700

    update seasons HTML structure

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ const imageQueryParams = '&fit=crop&w=640&h=990'
 
 
 
-fetch(`https://api.unsplash.com/search/photos?query=tokyo-spring&client_id=${key}`)
+// fetch(`https://api.unsplash.com/search/photos?query=tokyo-spring&client_id=${key}`)
     .then(res => res.json())
     .then(data => {
         const imageURL = `${data.results[Math.floor(Math.random() * 10)].urls.full}${imageQueryParams}`
