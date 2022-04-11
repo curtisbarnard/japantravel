a59d40f4e7562c5b2006d3fd9762020d09ddc642
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-04-10 21:19:48 -0700

    season nav styling switching to button

--- js/main.js
+++ js/main.js
@@ -1,47 +1,14 @@
-const key = 'wzKwRCJWvjjPxk8j1E9QeXs-zTIwtWSf7wcjI-rnFN8'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const key = 'wzKwRCJWvjjPxk8j1E9QeXs-zTIwtWSf7wcjI-rnFN8';
 
 const imageQueryParams = '&fit=crop&w=640&h=990'
 
+  // fetch(`https://api.unsplash.com/search/photos?query=tokyo-spring&client_id=${key}`
+  .then((res) => res.json())
+  .then((data) => {
+    const imageURL = `${
+      data.results[Math.floor(Math.random() * 10)].urls.full
+    }${imageQueryParams}`;
+    document.querySelector('.image').style.background = `url(${imageURL})`;
+  });
 
-
-// fetch(`https://api.unsplash.com/search/photos?query=tokyo-spring&client_id=${key}`)
-    .then(res => res.json())
-    .then(data => {
-        const imageURL = `${data.results[Math.floor(Math.random() * 10)].urls.full}${imageQueryParams}`
-        document.querySelector('.image').style.background = `url(${imageURL})`
-    })
+let test = 'heeeeey';
